a105416ea4b0187c022b05ac46dad232257f6233
Håkon Ellingsen <hakonweb@gmail.com>
2019-02-27 02:25:43 +0100

    Adjust colors and post hover

--- src/components/post-snippet.js
+++ src/components/post-snippet.js
@@ -7,11 +7,9 @@ export default ({ fields, frontmatter }) => (
   <article className={styles.postSnippet}>
     <Link to={fields.slug}>
       <h3 className={styles.title}>{frontmatter.title}</h3>
-
       <p className={styles.snippetLead}>{frontmatter.lead}</p>
-
-      <time className={styles.date}>{frontmatter.date}</time> &middot; {' '}
-      <span className={styles.more}>Read more &raquo;</span>
+      <time className={styles.date}>{frontmatter.date}</time> {' '}
+      <span className={styles.more}>&middot; Read more &raquo;</span>
     </Link>
   </article>
 )
