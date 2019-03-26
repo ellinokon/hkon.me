02058876c1d02142e2abf638804c8caf9a6625f9
Håkon Ellingsen <hakonweb@gmail.com>
2019-03-26 20:54:25 +0100

    Fix code styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: false,
-              noInlineHighlight: false,
+              noInlineHighlight: true,
             },
           },
         ],
